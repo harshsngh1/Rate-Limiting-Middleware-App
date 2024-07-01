003f81d1ef1ed336b6204d9db7ca9f48cfc57241
Harsh Singh <hs.harsh9919@gmail.com>
2024-07-02 00:23:49 +0530

    added endpoint to set rate limit

--- rate_limiting_middleware/handlers/rate_limiter_handler.go
+++ rate_limiting_middleware/handlers/rate_limiter_handler.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	"net/http"
+	"rate_limiting_middleware/config"
+
+	"github.com/labstack/echo/v4"
+)
+
+type RateLimitRequest struct {
+	Endpoint string `json:"endpoint,omitempty"`
+	IP       string `json:"ip,omitempty"`
+	Limit    int    `json:"limit"`
+}
+
+func SetRateLimit(c echo.Context) error {
+	req := new(RateLimitRequest)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "invalid request"})
+	}
+	if req.Endpoint == "" && req.IP == "" {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "either endpoint or IP must be specified"})
+	}
+
+	if req.IP == "" {
+		req.IP = "default"
+	}
+	config.RateLimiterConfig.SetRateLimit(req.Endpoint, req.IP, req.Limit)
+
+	return c.JSON(http.StatusOK, echo.Map{"message": "rate limit updated"})
+}
--- rate_limiting_middleware/routes/rate_limiter_route.go
+++ rate_limiting_middleware/routes/rate_limiter_route.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"rate_limiting_middleware/handlers"
+
+	"github.com/labstack/echo/v4"
+)
+
+func SetupRateLimitRoutes(e *echo.Echo) {
+	e.POST("/set-rate-limit", handlers.SetRateLimit)
+}
